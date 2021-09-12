0af288f8733af232fb168c05acf85b426cac79ef
Brais Cidras <braisomary@gmail.com>
2021-09-12 17:39:34 +0200

    Moar Functional Collections

---
+++ src/lectures/part2fp/MoarFunctionalCollections.scala
@@ -0,0 +1,12 @@
+package lectures.part2fp
+
+object MoarFunctionalCollections {
+
+  /*
+    Remember Seq and Map are partial functions
+    With certain index they return an element or throw an error
+      - if the index is bigger than the size - 1 of the Seq
+      - if the map does not contain such as provided key
+   */
+
+}
