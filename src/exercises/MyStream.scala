cf6cc3006a932ddb624d269e38b4d750c7435fa7
Brais Cidras <braisomary@gmail.com>
2021-09-19 16:20:05 +0200

    Infinite Streams Proficiency: More Exercises

--- src/exercises/MyStream.scala
+++ src/exercises/MyStream.scala
@@ -17,7 +17,7 @@ abstract class MyStream[+A] {
   def tail: MyStream[A]
 
   def #::[B >: A](element: B): MyStream[B] // Prepend operator
-  def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] // Concatenate two streams
+  def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] // Concatenate two streams
 
   def foreach(f: A => Unit): Unit
   def map[B](f: A => B): MyStream[B]
@@ -31,6 +31,7 @@ abstract class MyStream[+A] {
   final def toList[B >: A](acc: List[B] = Nil): List[B] =
     if (isEmpty) acc
     else tail.toList(head +: acc)
+//    else tail.toList(acc :+ head) // Required for proper order using Intructor's take method
 }
 
 object MyStream {
@@ -47,7 +48,7 @@ object EmptyStream extends MyStream[Nothing] {
 
   override def #::[B >: Nothing](element: B): MyStream[B] = new Cons(element, this)
 
-  override def ++[B >: Nothing](anotherStream: MyStream[B]): MyStream[B] = anotherStream
+  override def ++[B >: Nothing](anotherStream: => MyStream[B]): MyStream[B] = anotherStream // Remember this function should be by name so we could delay its evaluation is needed
 
   override def foreach(f: Nothing => Unit): Unit = ()
 
@@ -79,7 +80,7 @@ class Cons[+A](h: A, t: => MyStream[A]) extends MyStream[A] {
 
   override def #::[B >: A](element: B): MyStream[B] = new Cons(element, this)
 
-  override def ++[B >: A](anotherStream: MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
+  override def ++[B >: A](anotherStream: => MyStream[B]): MyStream[B] = new Cons(head, tail ++ anotherStream)
 
   override def foreach(f: A => Unit): Unit = {
     f(head)
@@ -99,15 +100,16 @@ class Cons[+A](h: A, t: => MyStream[A]) extends MyStream[A] {
     if (predicate(head)) new Cons(head, tail.filter(predicate))
     else tail.filter(predicate)
 
-  override def take(n: Int): MyStream[A] = {
-    @tailrec
-    def takeTailRec(n: Int, src: MyStream[A], acc: MyStream[A]):  MyStream[A] = {
-      if (n <= 0) acc
-      else takeTailRec(n - 1, src.tail, new Cons(src.head, acc))
+    override def take(n: Int): MyStream[A] = {
+      @tailrec
+      def takeTailRec(n: Int, src: => MyStream[A], acc: MyStream[A]):  MyStream[A] = {
+        if (n <= 0) acc
+        else takeTailRec(n - 1, src.tail, new Cons(src.head, acc))
+      }
+      takeTailRec(n, this, EmptyStream)
     }
-    takeTailRec(n, this, EmptyStream)
-  }
 
+  // Instructor's approach
 //  override def take(n: Int): MyStream[A] = {
 //    if (n <= 0) EmptyStream
 //    else if (n == 1) new Cons(head, EmptyStream)
@@ -115,7 +117,6 @@ class Cons[+A](h: A, t: => MyStream[A]) extends MyStream[A] {
 //  }
 
   override def takeList(n: Int): List[A] = take(n).toList()
-
 }
 
 object MyStreamPlayground extends App {
@@ -127,9 +128,43 @@ object MyStreamPlayground extends App {
   val startFrom0 = 0 #:: naturals // Naturals
   println(startFrom0.head)
 
-  startFrom0.take(10000).foreach(println)
+  //  startFrom0.take(10000).foreach(println)
 
   // map, flatMap
   println(startFrom0.map(_ * 2).take(100).toList())
   println(startFrom0.flatMap(x => new Cons(x, new Cons(x + 1, EmptyStream))).take(10).toList())
+  // If we only run the filter without the take, we will evaluate the filter on ALL the naturals, which will lead to a stack overflow
+  println(startFrom0.filter(_ < 10).take(10).toList())
+  // this will fail because the computer will try to find the next element naturals that is less than 10, which does not exist
+//    println(startFrom0.filter(_ < 10).take(11).toList())
+  // But this will work we will try to take more elements from an already fined 10 elements list. We the instructor's approach
+//    println(startFrom0.filter(_ < 10).take(10).take(20).toList())
+
+  /*
+    Exercises on Streams
+    1 - Stream of Fibonacci numbers
+    2 - Stream of Prime numbers with Eratosthenes' sieve
+      [ 2 3 4 ... ]
+      Filter out all numbers with Eratosthenes' sieve
+      [ 2 3 5 7 9 11 ... ]
+      Filter out all numbers divisible by 3
+      [ 2 3 5 7 11 13 17 ... ]
+      Filter out all numbers divisible by 5
+   */
+
+  /*
+    [ first, [ ...
+    [ first, fibonacci(second, first + second)
+   */
+  def fibonacci(first: Int, second: Int): MyStream[Int] =
+    new Cons(first, fibonacci(second, first + second))
+
+  println(fibonacci(1, 1).take(100).toList())
+
+  def primeNumbers(numbers: MyStream[Int]): MyStream[Int] = {
+    if (numbers.isEmpty) numbers
+    else new Cons(numbers.head, primeNumbers(numbers.tail.filter(_ % numbers.head != 0)))
+  }
+
+  println(primeNumbers(naturals.filter(_ != 1)).take(100).toList())
 }
