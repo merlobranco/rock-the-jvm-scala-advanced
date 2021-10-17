ede51b53d401f3cdc53a05a2dbfd730eeb27f394
Brais Cidras <braisomary@gmail.com>
2021-10-17 16:54:54 +0200

    Futures, Part 4 + Exercises

--- src/lectures/part3concurrency/FuturesPromises.scala
+++ src/lectures/part3concurrency/FuturesPromises.scala
@@ -2,7 +2,7 @@ package lectures.part3concurrency
 
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future, Promise}
-import scala.util.{Failure, Random, Success}
+import scala.util.{Failure, Random, Success, Try}
 
 // Important for Futures
 // ExecutionContext handles thread allocation of Futures
@@ -175,4 +175,127 @@ object FuturesPromises extends App {
     println("[Producer] done")
   })
   producer.start()
+
+  /*
+    EXERCISES
+    1) Fulfill a future IMMEDIATELY with a value
+    2) inSequence(fa, fb)
+    3) first(fa, fb) => new future with the first value of the 2 futures
+    4) last(fa, fb) => new future with the last value of the 2 futures
+    5) retryUntil[T](action: () => Future, condition: T => Boolean): Future[T]
+   */
+
+  // 1) Fulfill a future IMMEDIATELY with a value
+  def fulfillImmediately[T](value: T): Future[T] = Future(value)
+
+  // 2) inSequence(fa, fb)
+  def inSequence[A, B](fa: Future[A], fb: Future[B]): Future[B] =
+    fa.flatMap(_ => fb)
+
+  // 3) first(fa, fb) => new future with the first value of the 2 futures. First to finish
+  def first[T](fa: Future[T], fb: Future[T]): Future[T] = {
+    val promise = Promise[T]
+
+    // First approach
+//    fa.onComplete {
+//      case Success(v) => try promise.success(v)
+//      case Failure(e) => try promise.failure(e)
+//    }
+//
+//    fb.onComplete {
+//      case Success(v) => try promise.success(v)
+//      case Failure(e) => try promise.failure(e)
+//    }
+
+    // Second Approach
+//    def tryComplete(promise: Promise[T], result: Try[T]) = result match {
+//      case Success(v) => try promise.success(v)
+//      case Failure(e) => try promise.failure(e)
+//    }
+
+//    fa.onComplete(result => tryComplete(promise, result))
+//    fb.onComplete(result => tryComplete(promise, result))
+
+    // Third Approach
+//    fa.onComplete(tryComplete(promise, _))
+//    fb.onComplete(tryComplete(promise, _))
+
+    // Fourth Approach
+//    fa.onComplete(promise.tryComplete(_))
+//    fb.onComplete(promise.tryComplete(_))
+
+    // The first future to complete will succeed in calling tryComplete on the promise and on its result
+    // and that will set the resulting future with that result and the later future when calls onComplete
+    // the tryComplete will not do anything
+    fa.onComplete(promise.tryComplete)
+    fb.onComplete(promise.tryComplete)
+
+    // So at the end this guy will hold either the value or the exception returned by the first finished promise of those 2
+    promise.future
+
+  }
+
+  // 4) last(fa, fb) => new future with the last value of the 2 futures. Last to finish
+  def last[T](fa: Future[T], fb: Future[T]): Future[T] = {
+    // 1 promise which both futures will try to complete
+    // 2 promise which the LAST future will complete
+
+    val bothPromise = Promise[T]
+    val lastPromise = Promise[T]
+
+    // First Approach
+//    fa.onComplete(result => {
+//      if (!bothPromise.tryComplete(result))
+//        lastPromise.tryComplete(result)
+//    })
+//
+//    fb.onComplete(result => {
+//      if (!bothPromise.tryComplete(result))
+//        lastPromise.tryComplete(result)
+//    })
+
+    val checkAndComplete = (result: Try[T]) =>
+      if (!bothPromise.tryComplete(result))
+        lastPromise.tryComplete(result)
+
+    fa.onComplete(checkAndComplete)
+    fb.onComplete(checkAndComplete)
+
+    lastPromise.future
+  }
+
+  val fast = Future {
+    Thread.sleep(100)
+    42
+  }
+  val slow = Future {
+    Thread.sleep(200)
+    45
+  }
+
+  first(fast, slow).foreach(f => println(s"First: $f"))
+  last(fast, slow).foreach(l => println(s"Last: $l"))
+
+  // 5) retryUntil[T](action: () => Future, condition: T => Boolean): Future[T]
+  def retryUntil[T](action: () => Future[T], condition: T => Boolean): Future[T] =
+    action()
+      .filter(condition)
+      .recoverWith {
+        case _ => retryUntil(action, condition)
+      }
+
+  // If action filter condition is successful, then we just are going to return the that future,
+  // but in the case that future fails with NoSuchElementException, or some other exception
+  // is going to recover with, and in the case some throwable we don't actually care about,
+  // We'll just retry the action again
+
+  val random = Random
+  val action = () => Future {
+    Thread.sleep(100)
+    val nextValue = random.nextInt(100)
+    println(s"Generated $nextValue")
+    nextValue
+  }
+
+  retryUntil(action, (x: Int) => x > 50).foreach(result => println("Settled at " + result))
 }
